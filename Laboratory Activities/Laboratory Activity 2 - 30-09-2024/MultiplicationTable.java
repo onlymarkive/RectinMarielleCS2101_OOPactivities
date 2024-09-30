3a8a4036702202417102cf67ebb9207547d8b135
Marielle Rectin <157998090+onlymarkive@users.noreply.github.com>
2024-09-30 09:32:01 +0800

    Update MultiplicationTable.java

--- Laboratory Activities/Laboratory Activity 2 - 30-09-2024/MultiplicationTable.java
+++ Laboratory Activities/Laboratory Activity 2 - 30-09-2024/MultiplicationTable.java
@@ -1,33 +1,41 @@
 import java.util.Scanner;
 
 public class MultiplicationTable {
+    
+    public static int[][] getMultiplicationTable(int size) {
+        int[][] table = new int[size][size];
+        
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                table[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        
+        return table;
+    }
+    
+    public static void printMultiplicationTable(int[][] table) {
+        int size = table.length;
+
+        System.out.println("Multiplication Table:");
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.printf("%4d", table[i][j]);
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
         System.out.print("Enter the size of the multiplication table: ");
         int size = scanner.nextInt();
-
-        printMultiplicationTable(size);
-
+        
+        int[][] multiplicationTable = getMultiplicationTable(size);
+        
+        printMultiplicationTable(multiplicationTable);
+        
         scanner.close();
-    }
-
-    public static void printMultiplicationTable(int size) {
-        int[][] table = new int[size][size];
-
-        for (int i = 1; i <= size; i++) {
-            for (int j = 1; j <= size; j++) {
-                table[i - 1][j - 1] = i * j;
-            }
-        }
-
-        System.out.println("Multiplication Table:");
-        for (int i = 1; i <= size; i++) {
-            for (int j = 1; j <= size; j++) {
-                System.out.printf("%4d", table[i - 1][j - 1]);
-            }
-            System.out.println(); 
-        }
     }
 }
